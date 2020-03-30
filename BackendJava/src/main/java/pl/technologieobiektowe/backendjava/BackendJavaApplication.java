86a376a8badcec8d27d70241e5148534d9e6feb2
Damian Kozakowski <damian.kozakowski@outlook.com>
2020-03-30 22:00:49 +0200

    Inicjalizacja projektu backend Java

---
+++ BackendJava/src/main/java/pl/technologieobiektowe/backendjava/BackendJavaApplication.java
@@ -0,0 +1,13 @@
+package pl.technologieobiektowe.backendjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendJavaApplication.class, args);
+    }
+
+}
---
+++ BackendJava/src/test/java/pl/technologieobiektowe/backendjava/BackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.technologieobiektowe.backendjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
