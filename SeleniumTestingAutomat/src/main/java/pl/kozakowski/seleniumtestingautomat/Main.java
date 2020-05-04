c2bc62d75503dfbf819eeeff7078987cc0cd41b8
Damian Kozakowski <damian.kozakowski@outlook.com>
2020-05-04 22:44:50 +0200

    Małe fixy

--- SeleniumTestingAutomat/src/main/java/pl/kozakowski/seleniumtestingautomat/Main.java
+++ SeleniumTestingAutomat/src/main/java/pl/kozakowski/seleniumtestingautomat/Main.java
@@ -1,6 +1,8 @@
 package pl.kozakowski.seleniumtestingautomat;
 
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.TreeSet;
 
 public class Main {
@@ -19,8 +21,12 @@ public class Main {
         Set<Configuration.BROWSER> browsers = new TreeSet<>();
         browsers.add(Configuration.BROWSER.CHROME);
         browsers.add(Configuration.BROWSER.FIREFOX);
+//        browsers.add(Configuration.BROWSER.EDGE);
+//        browsers.add(Configuration.BROWSER.IE);
+//        browsers.add(Configuration.BROWSER.OPERA);
 
         TestsExecutor testsExecutor = new TestsExecutor(technologyClassExtractor.getInstalledTechnologyClasses(), browsers, reportGenerator);
         testsExecutor.executeTests();
+        reportGenerator.printReport();
     }
 }
