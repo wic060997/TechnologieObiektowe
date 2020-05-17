6483e1d1c7fea82ca594c2c1f7ebe9b71f80d540
MateuszBonar <mateusz211@vp.pl>
2020-05-17 10:58:26 +0200

    Dodanie CORSa

--- BackendJava/src/main/java/pl/technologieobiektowe/backendjava/TestsController.java
+++ BackendJava/src/main/java/pl/technologieobiektowe/backendjava/TestsController.java
@@ -2,6 +2,7 @@ package pl.technologieobiektowe.backendjava;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,21 +15,23 @@ public class TestsController {
         return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).body("I'am a Teapot");
     }
 
+
+    @CrossOrigin
     @RequestMapping(method = RequestMethod.GET, path = "/simple")
     public ResponseEntity simple() {
         return ResponseEntity.status(HttpStatus.OK).body("OK");
     }
-
+    @CrossOrigin
     @RequestMapping(method = RequestMethod.GET, path = "/data1k")
     public ResponseEntity data1k() {
         return ResponseEntity.status(HttpStatus.OK).body(TestData.getData1k());
     }
-
+    @CrossOrigin
     @RequestMapping(method = RequestMethod.GET, path = "/data10k")
     public ResponseEntity data10k() {
         return ResponseEntity.status(HttpStatus.OK).body(TestData.getData10k());
     }
-
+    @CrossOrigin
     @RequestMapping(method = RequestMethod.GET, path = "/data1m")
     public ResponseEntity data1m() {
         return ResponseEntity.status(HttpStatus.OK).body(TestData.getData1m());
