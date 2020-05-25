baa8cb383f6ce44590711c68d86181703a7508fb
MateuszBonar <mateusz211@vp.pl>
2020-05-25 22:55:54 +0200

    Dodanie CRUD

--- FrontReact/front/src/Components/CrudComponent.jsx
+++ FrontReact/front/src/Components/CrudComponent.jsx
@@ -1,41 +1,91 @@
 import React, { Component } from 'react';
 import '../App.css';
 
-const User = (props) => {
-  function deleteUser(id) {
-    alert(id);
-    console.log(this.state.users);
-  }
-  return (
-    <div className='element-user'>
-      <p>
-        {props.firstName} {props.lastName}
-      </p>
-      <p>{props.age}</p>
-      <input type='button' onClick={() => deleteUser(props.id)} value='Usun' />
-    </div>
-  );
-};
-
 class CrudComponent extends Component {
-  state = {
-    users: [{ id: 1, firstName: 'Mateusz', lastName: 'Bonar', age: 23 }],
+  constructor(props) {
+    super(props);
+    this.state = {
+      act: 0,
+      index: '',
+      users: [],
+    };
+  }
+
+  componentDidMount() {
+    this.refs.name.focus();
+  }
+
+  addData = (e) => {
+    e.preventDefault();
+    let users = this.state.users;
+    let name = this.refs.name.value;
+    let surname = this.refs.surname.value;
+
+    if (this.state.act === 0) {
+      let data = {
+        name,
+        surname: surname,
+      };
+      console.log(this.state.users);
+      users.push(data);
+    } else {
+      let index = this.state.index;
+      users[index].name = name;
+      users[index].surname = surname;
+    }
+
+    this.setState({
+      users: users,
+    });
+
+    this.refs.myForm.reset();
+    this.refs.name.focus();
+  };
+
+  removeData = (i) => {
+    let users = this.state.users;
+    users.splice(i, 1);
+    this.setState({
+      datas: users,
+    });
+
+    this.refs.myForm.reset();
+    this.refs.name.focus();
+  };
+
+  editData = (i) => {
+    let data = this.state.users[i];
+    this.refs.name.value = data.name;
+    this.refs.surname.value = data.surname;
+
+    this.setState({
+      act: 1,
+      index: i,
+    });
+    this.refs.name.focus();
   };
 
   render() {
-    const usersRender = this.state.users.map((item) => (
-      <User
-        firstName={item.firstName}
-        lastName={item.lastName}
-        age={item.age}
-        id={item.id}
-      ></User>
-    ));
     return (
-      <>
-        {usersRender}
-        <button>Dodaj kolejny</button>
-      </>
+      <div>
+        <h2>Lista użytkowników</h2>
+        <form ref='myForm'>
+          <input type='text' ref='name' placeholder='twoje imie' />
+          <input type='text' ref='surname' placeholder='twoje nazwisko' />
+          <button onClick={(e) => this.addData(e)}>Dodaj</button>
+        </form>
+        <pre>
+          {this.state.users.map((user, i) => (
+            <div key={i} className='div-user'>
+              <p className=''>
+                {i + 1}.{user.name}, {user.surname}
+              </p>
+              <button onClick={() => this.removeData(i)}>Usuń</button>
+              <button onClick={() => this.editData(i)}>Modyfikuj</button>
+            </div>
+          ))}
+        </pre>
+      </div>
     );
   }
 }
