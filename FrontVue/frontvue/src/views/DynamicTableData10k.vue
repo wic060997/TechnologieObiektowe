1e48f604c1f933a886cbd96bcac26e1ad6628220
Damian Kozakowski <damian.kozakowski@outlook.com>
2020-06-13 04:59:53 +0200

    Naprawa w DynamicTable Vue

--- FrontVue/frontvue/src/views/DynamicTableData10k.vue
+++ FrontVue/frontvue/src/views/DynamicTableData10k.vue
@@ -1,13 +1,32 @@
 <template>
     <div class="dynamic-table">
-      <p>Lorem gipsum Dynamic Table 10k Data</p>
+      <div id="text_area">
+        {{data}}
+      </div>
     </div>
 </template>
 
 <script>
 export default {
-  mounted() {
+  data() {
+    return {
+      data: {},
+    };
+  },
+
+  beforeMount() {
+    this.getText();
+  },
+
+  updated() {
     console.warn(`Stop:${Date.now()}`);
+  },
+  methods: {
+    async getText() {
+      const res = await fetch('http://localhost:8080/data10k');
+      const data = await res.json();
+      this.data = data;
+    },
   },
   name: 'DynamicTable10k',
 };
